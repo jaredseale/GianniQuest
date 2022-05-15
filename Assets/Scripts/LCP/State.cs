db87d0d169e54c8f1dfa73f11b98f3f779f38209
jaredseale <jared.seale@gmail.com>
2022-05-14 19:16:25 -0500

    assets for LCP gathered

---
+++ Assets/Scripts/LCP/State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class State : ScriptableObject
+{
+
+    [TextArea(14, 10)][SerializeField] string textArea;
+    [TextArea(14, 1)][SerializeField] string characterName;
+    public Sprite characterSprite;
+    public Sprite backgroundSprite;
+    public AudioClip backgroundMusic;
+    public AudioClip backgroundSFX;
+    public AudioClip beginningSFX;
+    public string[] choices;
+    [SerializeField] State[] nextStates;
+
+    void Start() {
+	
+        
+    }
+
+    void Update() {
+	
+        
+    }
+}
