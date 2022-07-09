3efa0694dceefc68ffa62a5d87eff212c75855bf
jaredseale <jared.seale@gmail.com>
2022-07-09 00:04:17 -0500

    overworld animations

---
+++ Assets/Scripts/Overworld/RancidRicksAnim.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RancidRicksAnim : MonoBehaviour
+{
+
+    Animator myAnim;
+
+    void Start() {
+        myAnim = GetComponent<Animator>();
+        if (PlayerPrefs.GetString("RicksEntry") == "Open") {
+            myAnim.SetTrigger("stinkLinesAppear");
+        }
+    }
+
+}
