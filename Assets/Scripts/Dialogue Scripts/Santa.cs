d51fd4b24cc2ab76136c189fea0e1c9e1a04bfdb
jaredseale <jared.seale@gmail.com>
2022-08-18 00:38:32 -0500

    track layering done in EA, also some art finished

---
+++ Assets/Scripts/Dialogue Scripts/Santa.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Santa : MonoBehaviour
+{
+
+    public DialogueTrigger currentDialogue;
+    public DialogueManager dialogueManager;
+
+    string[] Init = new string[] {"Ho ho ho, my dear Gianni, what a Christmas disaster!",
+        "Here I am, flying through the air in this winter wonderland you call Arizona, when one of my reindeer gets sick and careens us into a telephone pole.",
+        "My magical bag got caught on a transformer and ripped, and I lost some of my gifts for all you good boys and girls out there.",
+        "I don't suppose you would mind lending me a hand while I tend to my reindeer?",
+        "I took inventory a short while ago, and I believe I'm missing twenty presents."};
+    string[] Init2 = new string[] { "I'm still missing a few of the presents I dropped. Have a look around and see if you can spot any." };
+    string[] PreDollar = new string[] { "Ho ho ho, thank you so much, Gianni! That looks like everything that fell out of my bag.",
+        "Although on second thought, this rusty key doesn't seem to belong to me. I'll let you keep that one.",
+        "And since you've been an extra good boy this year, why don't I reward you with a crisp United States one dollar bill that my elves made? Don't tell your government!"};
+    string[] PostDollar = new string[] { "My reindeer look to be in ship-shape now. I blame that hot dog they ate earlier." };
+
+    private void Start() {
+        dialogueManager = FindObjectOfType<DialogueManager>();
+        SetDialogue();
+    }
+
+    private void Update() {
+
+        if (dialogueManager.dialogueTarget == this.gameObject) {
+            if (dialogueManager.actionTrigger == true) {
+                dialogueManager.actionTrigger = false;
+                PlayAction();
+            }
+        }
+
+        SetDialogue();
+
+        void PlayAction() {
+
+            if (PlayerPrefs.GetString("SantaDialogueState") == "Init") {
+                PlayerPrefs.SetString("SantaDialogueState", "Init2");
+            } else if (PlayerPrefs.GetString("SantaDialogueState") == "PreDollar") {
+                PlayerPrefs.SetString("SantaDialogueState", "PostDollar");
+                PlayerPrefs.SetInt("EADataManagement", 0);
+                Dollar myDollar = FindObjectOfType<Dollar>();
+                myDollar.CollectDollar();
+            }
+        }
+    }
+
+    private void SetDialogue() {
+        if (PlayerPrefs.GetString("SantaDialogueState") == "Init") {
+            currentDialogue.dialogue.sentences = Init;
+        } else if (PlayerPrefs.GetString("SantaDialogueState") == "Init2") {
+            currentDialogue.dialogue.sentences = Init2;
+        } else if (PlayerPrefs.GetString("SantaDialogueState") == "PreDollar") {
+            currentDialogue.dialogue.sentences = PreDollar;
+        } else if (PlayerPrefs.GetString("SantaDialogueState") == "PostDollar") {
+            currentDialogue.dialogue.sentences = PostDollar;
+        }
+    }
+}
---
+++ Assets/Scripts/Ethereal Ascent/EAMusicManager.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EAMusicManager : MonoBehaviour
+{
+
+    [SerializeField] AudioSource track2;
+    [SerializeField] AudioSource track3;
+    [SerializeField] AudioSource track4;
+    [SerializeField] AudioSource track5;
+    [SerializeField] AudioSource track6;
+
+    float track2Threshold = 20f; //these will obviously need to be adjusted as the level is developed
+    float track3Threshold = 40f;
+    float track4Threshold = 60f;
+    float track5Threshold = 80f;
+    float track6Threshold = 100f;
+
+    float fadeTimer = 4f;
+
+    Player myPlayer;
+
+    void Start() {
+        myPlayer = FindObjectOfType<Player>();
+
+        track2.volume = 0f;
+        track3.volume = 0f;
+        track4.volume = 0f;
+        track5.volume = 0f;
+        track6.volume = 0f;
+    }
+
+    void Update() {
+        FadeInMusic();
+        FadeOutMusic();
+    }
+
+    void FadeInMusic() {
+
+        if (track2.volume <= .001f && myPlayer.transform.position.y > track2Threshold) {
+            StartCoroutine(FadeInTrack(track2));
+        }
+
+        if (track3.volume <= .001f && myPlayer.transform.position.y > track3Threshold) {
+            StartCoroutine(FadeInTrack(track3));
+        }
+
+        if (track4.volume <= .001f && myPlayer.transform.position.y > track4Threshold) {
+            StartCoroutine(FadeInTrack(track4));
+        }
+
+        if (track5.volume <= .001f && myPlayer.transform.position.y > track5Threshold) {
+            StartCoroutine(FadeInTrack(track5));
+        }
+
+        if (track6.volume <= .001f && myPlayer.transform.position.y > track6Threshold) {
+            StartCoroutine(FadeInTrack(track6));
+        }
+
+    }
+
+    void FadeOutMusic() {
+
+        if (track2.volume >= .999f && myPlayer.transform.position.y < track2Threshold) {
+            StartCoroutine(FadeOutTrack(track2));
+        }
+
+        if (track3.volume >= .999f && myPlayer.transform.position.y < track3Threshold) {
+            StartCoroutine(FadeOutTrack(track3));
+        }
+
+        if (track4.volume >= .999f && myPlayer.transform.position.y < track4Threshold) {
+            StartCoroutine(FadeOutTrack(track4));
+        }
+
+        if (track5.volume >= .999f && myPlayer.transform.position.y < track5Threshold) {
+            StartCoroutine(FadeOutTrack(track5));
+        }
+
+        if (track6.volume >= .999f && myPlayer.transform.position.y < track6Threshold) {
+            StartCoroutine(FadeOutTrack(track6));
+        }
+
+    }
+
+    IEnumerator FadeInTrack(AudioSource track) {
+        float currentTime = 0;
+        float currentVol = track.volume;
+        float targetValue = 1f;
+
+        while (currentTime < fadeTimer) {
+            currentTime += Time.deltaTime;
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / fadeTimer);
+            track.volume = newVol;
+            yield return null;
+        }
+        yield break;
+    }
+
+    IEnumerator FadeOutTrack(AudioSource track) {
+        float currentTime = 0;
+        float currentVol = track.volume;
+        float targetValue = 0f;
+
+        while (currentTime < fadeTimer) {
+            currentTime += Time.deltaTime;
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / fadeTimer);
+            track.volume = newVol;
+            yield return null;
+        }
+        yield break;
+    }
+}
