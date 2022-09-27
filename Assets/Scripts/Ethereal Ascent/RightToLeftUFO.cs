523af3ed83fb244ecd4fea61b629d4b0eaf9fca3
jaredseale <jared.seale@gmail.com>
2022-09-27 00:21:45 -0500

    main level of EA done
    still need to do stuff like save management

--- Assets/Scripts/Ethereal Ascent/RightToLeftUFO.cs
+++ Assets/Scripts/Ethereal Ascent/RightToLeftUFO.cs
@@ -16,20 +16,39 @@ public class RightToLeftUFO : MonoBehaviour
         int colorPicker = Random.Range(0, 4);
 
         if (colorPicker == 0) {
-            mySpriteRenderer.color = new Color(0f, 1f, 1f);
+            mySpriteRenderer.color = new Color(Random.Range(0f, 0.5f), 1f, 1f);
         } else if (colorPicker == 1) {
-            mySpriteRenderer.color = new Color(1f, 0f, 1f);
+            mySpriteRenderer.color = new Color(1f, Random.Range(0f, 0.5f), 1f);
         } else if (colorPicker == 2) {
-            mySpriteRenderer.color = new Color(1f, 1f, 0f);
+            mySpriteRenderer.color = new Color(1f, 1f, Random.Range(0f, 0.5f));
         } else if (colorPicker == 3) {
             mySpriteRenderer.color = new Color(1f, 1f, 1f);
         }
+
+        StartCoroutine(Vector3LerpCoroutine(gameObject, new Vector3(-65f, gameObject.transform.position.y, 0f), moveSpeed));
     }
 
     void Update() {
-        myRigidbody.velocity = new Vector2(-moveSpeed, myRigidbody.velocity.y);
-        if (gameObject.transform.position.x < -80f) {
+        if (gameObject.transform.position.x < -60f) {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator Vector3LerpCoroutine(GameObject obj, Vector3 target, float speed) {
+        Vector3 startPosition = obj.transform.position;
+        float time = 0f;
+
+        while (obj.transform.position != target) {
+            obj.transform.position = Vector3.Lerp(startPosition, target, (time / Vector3.Distance(startPosition, target)) * speed);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col) {
+        col.gameObject.transform.SetParent(gameObject.transform, true);
+    }
+
+    void OnCollisionExit2D(Collision2D col) {
+        col.gameObject.transform.parent = null;
     }
 }
