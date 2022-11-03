086d16c3ee6e470a5cfc0fb122392ae27eee29f7
jaredseale <jared.seale@gmail.com>
2022-11-03 00:17:48 -0500

    added spritesheets for enemies, created first room of sewers
    well i started the first room anyway

--- Assets/Scripts/MapLocationText.cs
+++ Assets/Scripts/MapLocationText.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class MapLocationText : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI locationName;
+    [SerializeField] SpriteRenderer locationColor;
     [SerializeField] OverworldPlayer player;
     [SerializeField] GameObject locationDisplay;
 
@@ -26,6 +27,7 @@ public class MapLocationText : MonoBehaviour
 
             case "A":
                 locationName.text = "Le Cul Puant";
+                locationColor.color = new Color(200/255f, 106/255f, 0/255f);
 
                 if (PlayerPrefs.GetString("DateProgress") == "End") {
                     locationName.text = "GBGL-XX1";
@@ -34,30 +36,37 @@ public class MapLocationText : MonoBehaviour
 
             case "C1":
                 locationName.text = "Pizza Hell";
+                locationColor.color = new Color(0f, 0f, 0f);
                 break;
 
             case "J":
                 locationName.text = "Rancid Rick's";
+                locationColor.color = new Color(0f, 200/255f, 33/255f);
                 break;
 
             case "N4":
                 locationName.text = "Ethereal Ascent";
+                locationColor.color = new Color(0f, 148/255f, 200/255f);
                 break;
 
             case "S":
                 locationName.text = "Home House";
+                locationColor.color = new Color(0f, 0f, 0f);
                 break;
 
             case "T":
                 locationName.text = "The Sewers";
+                locationColor.color = new Color(55/255f, 0f, 206/255f);
                 break;
 
             case "V":
                 locationName.text = "High School High";
+                locationColor.color = new Color(200/255f, 212/255f, 0f);
                 break;
 
             case "W":
                 locationName.text = "SNICO";
+                locationColor.color = new Color(200/255f, 0f, 0f);
                 break;
         }
     }
---
+++ Assets/Scripts/Sewers/SewersMusicManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SewersMusicManager : MonoBehaviour
+{
+
+    public AudioSource fullTrack;
+    public AudioSource loopTrack;
+
+    void Start() {
+	    
+        
+    }
+
+    void Update() {
+        if (fullTrack.isPlaying == false) {
+            fullTrack.gameObject.SetActive(false);
+            loopTrack.gameObject.SetActive(true);
+        }
+        
+    }
+}
